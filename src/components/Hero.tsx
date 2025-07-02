0fd1f2cfb19022f6ebe0dad587e38f619cbc0c1a
hwangtab <hwangtab@gmail.com>
2025-07-02 16:25:28 +0900

    fix: Hero.tsx 타입 오류 수정

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -281,7 +281,7 @@ function throttle<T extends (...args: any[]) => any>(func: T, limit: number): T 
   let inThrottle: boolean
   return ((...args: any[]) => {
     if (!inThrottle) {
-      func.apply(this, args)
+      func(...args)
       inThrottle = true
       setTimeout(() => inThrottle = false, limit)
     }
@@ -292,7 +292,7 @@ function debounce<T extends (...args: any[]) => any>(func: T, delay: number): T 
   let timeoutId: NodeJS.Timeout
   return ((...args: any[]) => {
     clearTimeout(timeoutId)
-    timeoutId = setTimeout(() => func.apply(this, args), delay)
+    timeoutId = setTimeout(() => func(...args), delay)
   }) as T
 }
 
